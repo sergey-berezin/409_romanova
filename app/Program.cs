9864dda80306a8fcc9ece49cab3de02058f8bcde
Yuliya Romanova <amor.avon@yandex.ru>
2024-09-12 22:52:25 -0700

    lab

---
+++ app/Program.cs
@@ -0,0 +1,12 @@
+﻿using PackagingGenetic;
+
+class Program
+{
+    static void Main()
+    {
+        int N1 = 2, N2 = 2, N3 = 1;
+        Tuple<int, int> Answer = PackagingSolver.GeneticAlgorithm(N1, N2, N3);
+        Console.WriteLine("Best square: " + Answer.Item1);
+        Console.WriteLine("Number of generation: " + Answer.Item2);
+    }
+}
---
+++ lib/Constants.cs
@@ -0,0 +1,9 @@
+namespace PackagingGenetic;
+
+public static class Constants
+{
+    public const int POPULATION_SIZE = 50;
+    public const int RANDOM_STOP_CNT = 500;
+    public const int MAX_FIELD_MULT = 3;
+    public const int MAX_ITEM_SIZE = 3;
+}
